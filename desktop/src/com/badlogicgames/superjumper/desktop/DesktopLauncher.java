189ccf14c54cd858730be43116e81936a1b5a190
saedarm <searmentrout@gmail.com>
2026-06-27 09:18:03 -0400

    updated to the 21st century

--- desktop/src/com/badlogicgames/superjumper/desktop/DesktopLauncher.java
+++ desktop/src/com/badlogicgames/superjumper/desktop/DesktopLauncher.java
@@ -1,15 +1,16 @@
 package com.badlogicgames.superjumper.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogicgames.superjumper.SuperJumper;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Super Jumper";
-		config.width = 480;
-		config.height = 800;
-		new LwjglApplication(new SuperJumper(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Super Jumper");
+		config.setWindowedMode(480, 800);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new SuperJumper(), config);
 	}
 }
